70ab036aa52afad85771963161eaf403b9294e5d
cyphex <cyphex@spiritcore.de>
2016-05-29 19:36:44 +0200

    Make setup script work outside of .vim directory.

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ ! -d "./bundles" ]]; then
-    git clone git://github.com/VundleVim/Vundle.vim.git ./bundle/Vundle.vim
-    vim -u bundles.vim +PluginInstall +qall
+if [[ ! -d "~/.vim/bundles" ]]; then
+    git clone git://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim -u ~/.vim/bundles.vim +PluginInstall +qall
 else
     echo "Vundle appears to be already initialized, stopping."
 fi
